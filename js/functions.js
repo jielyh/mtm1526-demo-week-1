4cb42757f4f882fbf1bb39580654b71b75575b73
jielyh <hyper_lil_angel_905@hotmail.com>
2012-01-13 11:49:34 -0500

    renamed functions file

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+// javascript single line comment
+/*
+Javascript multi-line comment
+*/
+
+// 'alert()' pops up a little window with okay button
+alert('this is an alert.');
+
+
+// 'prompt()' is for users, has a text field for the user to enter some text
+var name = prompt('what is your name?');
+
+//tace() and consol.log() = same thing
+//'console.log()' writes outs the content of a variable for debugging
+// identical to Actionscript's 'trace()'
+consol.log (name);
