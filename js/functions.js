90c3e8f36bc7aa78b8ccd8e816617053cd954720
jielyh <hyper_lil_angel_905@hotmail.com>
2012-01-13 12:29:58 -0500

    modified

--- js/functions.js
+++ js/functions.js
@@ -6,11 +6,44 @@ Javascript multi-line comment
 // 'alert()' pops up a little window with okay button
 alert('this is an alert.');
 
-
 // 'prompt()' is for users, has a text field for the user to enter some text
-var name = prompt('what is your name?');
+var name;
+while (!name) {
+name = prompt('what is your name?');
+}
 
 //tace() and consol.log() = same thing
 //'console.log()' writes outs the content of a variable for debugging
 // identical to Actionscript's 'trace()'
-consol.log (name);
+console.log (name);
+
+//'document.write()' allows us to output stuff to the end of the HTML file
+// the '+' operator is for combining strings and together
+document.write('Hello, ' + name);
+
+
+// (i) is a variable we made to determine the number of loops
+for (var i = 0; i < 5; i++) {
+	document.write('<br>' + i); //<br> = new line for every + new number
+	document.write('<h1>' + i + '<h1>');
+}
+
+function isNameCool (){
+	var name = prompt ('What is your name?');
+	
+	if (name == 'jiely') {
+			document.write('That is an awesome name!');
+	} else {
+			document.write('I dont like your name');
+	}
+}
+
+isNameCool(); //making the function
+
+var writeNameMultipleTimes = function () {
+	for (var i = 0; i < 5; i++ ) {
+		document.write('<strong> Jiely </strong> <br>');
+	}
+};
+
+writeNameMultipleTimes ();
